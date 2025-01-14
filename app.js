a5e9a5bddec4b10032aff247562aa50a5c07353f
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-01-14 10:54:25 +0700

    chore: add PORT to .env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
